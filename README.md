fc91db14c1937f86c92d094913ea67c2921f0332
Denis Zolkin <dezolkin@gmail.com>
2018-09-12 00:05:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Description
+This is a repo with code for a study project of file system.
+[Rock the JVM! Scala and Functional Programming for Beginners](https://www.udemy.com/rock-the-jvm-scala-for-beginners/)
---
+++ src/org/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -0,0 +1,13 @@
+package org.rtjvm.scala.oop.filesystem
+
+import java.util.Scanner
+
+object Filesystem extends App {
+
+  val scanner = new Scanner(System.in)
+
+  while(true) {
+    println("$ ")
+    println(scanner.nextLine())
+  }
+}
