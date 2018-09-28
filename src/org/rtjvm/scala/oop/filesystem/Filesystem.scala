8bbec50bdfb83ea51ba36c257f7687bd88a40e1b
Denis Zolkin <dezolkin@gmail.com>
2018-09-28 18:56:31 +0300

    manage state with fold

--- src/org/rtjvm/scala/oop/filesystem/Filesystem.scala
+++ src/org/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -1,20 +1,15 @@
 package org.rtjvm.scala.oop.filesystem
 
-import java.util.Scanner
-
 import org.rtjvm.scala.oop.commands.Command
 import org.rtjvm.scala.oop.files.Directory
 
 object Filesystem extends App {
 
   val root = Directory.ROOT
-  var state = State(root, root)
-  val scanner = new Scanner(System.in)
 
-  while(true) {
-    state.show
-
-    val input = scanner.nextLine()
-    state = Command.from(input).apply(state)
-  }
+  io.Source.stdin.getLines().
+    foldLeft(State(root, root))((currentState, newLine) => {
+      currentState.show
+      Command.from(newLine).apply(currentState)
+    })
 }
